6d8541d58c4c742815b0da07d36f6d87de2bc334
Declan Murphy <declxn@gmail.com>
2021-05-02 15:56:18 -0500

    Created fetchWeather api request, exported the constant to the main app and set up css.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { fetchWeather } from './api/fetchWeather';
+import './Api.css';
+
+const App = () => {
+  return (
+    <h1>Hi I Am App</h1>
+  );
+}
+
+export default App;
