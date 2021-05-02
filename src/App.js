c9a4ee36f145833822ff91d37ce8b23f37e8bdbd
Declan Murphy <declxn@gmail.com>
2021-05-02 16:14:43 -0500

    Fixed broken css, found new link for background. Now displaying properly.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { fetchWeather } from './api/fetchWeather';
-import './Api.css';
+import './App.css';
 
 const App = () => {
+  const [query, setQuery] = useState('');
+
+  const search = async (e) => {
+    if (e.key === 'Enter') {
+      const data = await fetchWeather(query)
+
+      console.log(data)
+    }
+  }
+
   return (
-    <h1>Hi I Am App</h1>
+    <div className="main-container">
+      <input
+        type="text"
+        className="search"
+        placeholder="Search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
+    </div>
   );
 }
 
