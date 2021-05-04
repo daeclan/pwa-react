a85ca2a5fbfe8320bb08b6784583c912b0475028
Declan Murphy <declxn@gmail.com>
2021-05-04 12:38:36 -0500

    Added the cache to the PWA, allowing for both online and offline access.

---
+++ public/serviceworker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "verison-1"
+const urlsToCache = ['index.html', 'offline.html']
+const self = this;
+
+//Install SW
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log("opened cache")
+
+        return cache.addAll(urlsToCache)
+      })
+  )
+});
+
+//Listen for requests
+
+self.addEventListener('fetch', (event) => {
+
+});
+
+//Activate the SW
+self.addEventListener('activate', (event) => {
+
+});
