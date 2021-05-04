58a2fe9a016512e75e284c20a000ff580060104e
Declan Murphy <declxn@gmail.com>
2021-05-04 13:04:24 -0500

    Set up the service worker with promises and .thens inside of event.waitUntil's, utilizing React's cache abilities.

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -18,10 +18,26 @@ self.addEventListener('install', (event) => {
 //Listen for requests
 
 self.addEventListener('fetch', (event) => {
-
+  event.respondWith(
+    caches.match(event.request)
+      .then(() => {
+        return fetch(event.request)
+          .catch(() => caches.match('offline.html'))
+      })
+  )
 });
 
 //Activate the SW
 self.addEventListener('activate', (event) => {
-
+  const cacheWhitelist = [];
+  cacheWhitelist.push(CACHE_NAME);
+  event.waitUntil(
+    caches.keys().then((cacheNames) => Promise.all(
+      cacheNames.map((cacheName) => {
+        if (!cacheWhitelist.includes(cacheName)) {
+          return caches.delete(cacheName)
+        }
+      })
+    ))
+  )
 });
